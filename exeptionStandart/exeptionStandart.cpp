4dfbd51c4ae66be849000c1e86be9bde5de83548
farisabqari <127277279+farisabqari@users.noreply.github.com>
2023-06-21 12:31:14 +0800

    menambahkan cout, try, catch

--- exeptionStandart/exeptionStandart.cpp
+++ exeptionStandart/exeptionStandart.cpp
@@ -7,6 +7,19 @@ using namespace std;
 
 int main()
 {
-
+	cout << "Awal program" << endl; //penanda 1:...
+	try {
+		array<int, 3> data = { 1, 2, 3 };
+		//pesan array integer 3 elemen
+		cout << data.at(4) << endl;
+		//memanggil array elemen ke 5
+	}
+	catch (exception& e) {
+		//penangkap menggunakan obyek exception
+		cout << e.what() << endl;
+		/*akan dieksekusi karna array data hanya memiliki 3 elemen*/
+	}
+	cout << "Baris Program yang terakhir" << endl;
+	/*penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
 
 }
